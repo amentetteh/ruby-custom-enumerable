58688b3405a41d6af66091db439c586c7e12ec9b
Amen TETTEH <tkamen@outlook.com>
2023-02-03 19:42:17 +0000

    Add robocop.yml file

---
+++ my_list.rb
@@ -0,0 +1,17 @@
+require_relative './my_custom_enumerable'
+
+class MyList
+
+  def initialize(*args)
+    @list = args
+  end
+  def each(&block)
+    @list.each(&block)
+  end   
+end
+list = MyList.new(1, 2, 3, 4)
+list.all? {|e| e < 5}
+list.all? {|e| e > 5}
+list.any? {|e| e == 2}
+list.any? {|e| e == 5}
+list.filter {|e| e.even?}
