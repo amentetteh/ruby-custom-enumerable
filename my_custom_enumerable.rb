3cc61d70541d4a70ae7e73e939d4a9421ab3a7ad
Vanessa O <vaneolipa@gmail.com>
2023-02-03 15:11:42 -0500

    Create module MyEnumerable

--- my_custom_enumerable.rb
+++ my_custom_enumerable.rb
@@ -1,1 +1,17 @@
-module Enumerable
+module MyEnumerable
+  def all?
+    each { |item| return false unless yield item }
+    true
+  end
+
+  def any?
+    each { |item| return true if yield item }
+    false
+  end
+
+  def filter
+    filter_array = []
+    each { |item| filter_array.push(item) if yield item }
+    filter_array
+  end
+end
