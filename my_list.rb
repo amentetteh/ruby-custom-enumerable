5f028103efa429766ebe506ce58aede1b2a7ceec
Vanessa O <vaneolipa@gmail.com>
2023-02-03 15:13:30 -0500

    Fix linters errors

--- my_list.rb
+++ my_list.rb
@@ -5,13 +5,14 @@ class MyList
   def initialize(*args)
     @list = args
   end
+
   def each(&block)
     @list.each(&block)
-  end   
+  end
 end
 list = MyList.new(1, 2, 3, 4)
-list.all? {|e| e < 5}
-list.all? {|e| e > 5}
-list.any? {|e| e == 2}
-list.any? {|e| e == 5}
-list.filter {|e| e.even?}
+list.all? { |e| e < 5 }
+list.all? { |e| e > 5 }
+list.any? { |e| e == 2 }
+list.any? { |e| e == 5 }
+list.filter(&:even?)
